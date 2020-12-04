5cd2d25c308f97dc49eb798b599889259ba800bc
longspringcity <1277535055@qq.com>
2020-12-04 14:26:47 +0800

    small changes

--- reg_train.py
+++ reg_train.py
@@ -152,6 +152,8 @@ def train():
     visual_dataloader = DataLoader(visual_test_dataset, batch_size=1, num_workers=opt.num_workers, pin_memory=True)
     for i, data in enumerate(visual_dataloader):
         raw_rgb, rgb, real_pos = data
+        print(raw_rgb)
+        print(raw_rgb.shape)
         rgb = rgb.cuda()
         real_pos = real_pos.cuda()
         pred_pos = model(rgb)
